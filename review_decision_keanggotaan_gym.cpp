b51369f44d440b11b96f102f50f74fbe646dce46
aydnr <ayudinarovani@gmail.com>
2025-02-03 20:49:15 -0800

    Update review_decision_keanggotaan_gym.cpp

--- review_decision_keanggotaan_gym.cpp
+++ review_decision_keanggotaan_gym.cpp
@@ -1,21 +1,37 @@
 //6. sistem keanggotaan gym
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    string anggota, platinum, silver, gold;
-    int jumlah;
-    cout<<"Masukan jenis keanggotaan anda: ";
-    cin>>anggota;
-    cout<<"Masukan jumlah latihan yang dilakukan: ";
-    cin>>jumlah;
-    if(anggota=="silver"){
-        cout<<"Anda masih bisa latihan sebanyak "<<10-jumlah<<"X lagi bulan ini"<<endl;
-    }else if(anggota=="gold"){
-        cout<<"Anda masih bisa latihan sebanyak "<<20-jumlah<<"X lagi bulan ini"<<endl;
-    }else if(anggota=="platinum"){
-        cout<<"Silahkan latihan kapanpun"<<endl;
-    }else{
-        
+int main() {
+    
+    string jenisKeanggotaan;
+    int jumlahLatihan, batasLatihan;
+    
+    cout << "Masukkan Jenis Keanggotaan ( silver/gold/platinum ) : ";
+    cin >> jenisKeanggotaan;
+    
+    if ( jenisKeanggotaan == "silver" ) {
+        batasLatihan = 10;
+    } else { 
+         if ( jenisKeanggotaan == "gold" ) {
+            batasLatihan = 20;
+    } else {
+         if ( jenisKeanggotaan == "platinum" ) {
+            cout << "Anda Tidak memiliki batas\n";
+            return 0;
+            } else {
+                cout << "Tidak valid!\n";
+            } return 0;
+        }
     }
+     
+   cout << "Masukkan jumlah latihan yang dilakukan : ";
+   cin >> jumlahLatihan;
+   
+   if ( jumlahLatihan < batasLatihan) {
+       cout << "Anda masih bisa latihan sebanyak " << (batasLatihan - jumlahLatihan) << " kali lagi bulan ini\n";
+   } else {
+       cout << "Anda sudah mencapai batas latihan bulan ini\n";
+   }
+    return 0;
 }
