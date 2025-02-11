f63fc911fcc8750d817514834c6f79f997764cd3
aydnr <ayudinarovani@gmail.com>
2025-02-10 17:48:45 -0800

    Update review_decision_keanggotaan_gym.cpp

--- review_decision_keanggotaan_gym.cpp
+++ review_decision_keanggotaan_gym.cpp
@@ -1,5 +1,7 @@
 //6. sistem keanggotaan gym
 
+//cara pertama 
+
 #include <iostream>
 using namespace std;
 int main() {
@@ -25,6 +27,41 @@ int main() {
         }
     }
      
+   cout << "Masukkan jumlah latihan yang dilakukan : ";
+   cin >> jumlahLatihan;
+   
+   if ( jumlahLatihan < batasLatihan) {
+       cout << "Anda masih bisa latihan sebanyak " << (batasLatihan - jumlahLatihan) << " kali lagi bulan ini\n";
+   } else {
+       cout << "Anda sudah mencapai batas latihan bulan ini\n";
+   }
+    return 0;
+}
+
+
+//cara kedua
+
+#include <iostream>
+using namespace std;
+int main() {
+    
+    string jenisKeanggotaan;
+    int jumlahLatihan, batasLatihan;
+    
+    cout << "Masukkan Jenis Keanggotaan ( silver/gold/platinum ) : ";
+    cin >> jenisKeanggotaan;
+    
+    if ( jenisKeanggotaan == "silver" ) {
+        batasLatihan = 10;
+    } else if ( jenisKeanggotaan == "gold" ) {
+            batasLatihan = 20;
+    } else if( jenisKeanggotaan == "platinum" ) {
+            cout << "Anda Tidak memiliki batas\n";
+            return 0;
+            } else {
+                cout << "Tidak valid!\n";
+            } 
+         
    cout << "Masukkan jumlah latihan yang dilakukan : ";
    cin >> jumlahLatihan;
    
