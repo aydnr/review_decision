5f7c19c039f27bf2db0c41e8155db0d799616e7c
aydnr <ayudinarovani@gmail.com>
2025-02-10 17:50:40 -0800

    Update review_decision_lampu_lalu_lintas.cpp

--- review_decision_lampu_lalu_lintas.cpp
+++ review_decision_lampu_lalu_lintas.cpp
@@ -1,4 +1,7 @@
-// 3. Lampu lalu lintas 
+// 3. Lampu lalu lintas
+
+//cara pertama
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -16,4 +19,24 @@ int main(){
     }else{
         cout<<"Warna tidak valid"<<endl;
     }}}
+}
+
+//cara kedua
+
+#include<iostream>
+using namespace std;
+int main(){
+    string warna, merah, kuning, hijau;
+    cout<<"Masukan warna = ";
+    cin>>warna;
+    if(warna=="merah"){
+        cout<<"Berhenti!"<<endl;
+    }else if(warna=="kuning"){
+        cout<<"Bersiap!"<<endl;
+    
+    }else if(wanrna=="hijau"){
+        cout<<"Jalan"<<endl;
+    }else{
+        cout<<"Warna tidak valid"<<endl;
+    }
 }
